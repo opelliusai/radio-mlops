0d1989c0c62c2faecee6136f2a999dea0e2f6866
opelliusai <jelgasmi@gmail.com>
2024-09-10 00:22:09 +0200

    Mise à jour Docker compose avec une version de pull des images depuis dockerhub

--- src/streamlit_pres/pages/3_Demo.py
+++ src/streamlit_pres/pages/3_Demo.py
@@ -14,3 +14,8 @@ st.write("-Profil Contributeur")
 st.write("--Contribution")
 
 st.write("-Profil Administrateur")
+st.write("--Etat des services")
+st.write("--Suivi des performances du modèle en production")
+st.write("--Entrainement/Réentrainement")
+st.write("--Informations et déploiement de modèles")
+st.write("--Informations et déploiement de modèles")
